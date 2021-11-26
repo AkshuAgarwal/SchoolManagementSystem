fc0bcc88e443378d0b22ecc5a9e7cad56f6007ff
Akshu <akshitagarwal2204@gmail.com>
2021-11-26 13:13:21 +0530

    Create Authorize route

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from . import views
+
+
 urlpatterns = [
-    ...,
+    path("authorize/", views.AuthViewSet.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+from administration.models import AuthorizationModel, UserModel
+from utils import errors as e
+
+
+class AuthViewSet(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        data = request.data
+
+        try:
+            email_id = data.get("email_id")
+            username = data.get("username")
+            password = data.get("password")
+
+            if not password:
+                return e.HTTP400Response()
+
+            if username:
+                user = authenticate(username=username, password=password)
+            elif email_id:
+                u = UserModel.objects.get(email_id=email_id)
+                user = authenticate(username=u.username, password=password)
+
+            else:
+                return e.HTTP400Response()
+
+            if not user:
+                return e.HTTP404Response("No user found with the given credentials")
+
+            auth = AuthorizationModel.objects.get(user=user)
+
+            return Response(
+                {
+                    "username": user.username,
+                    "email_id": user.email_id,
+                    "authorization_token": auth.authorization_token,
+                },
+                status=status.HTTP_200_OK,
+            )
+
+        except ObjectDoesNotExist:
+            return e.HTTP404Response("No user found with the given credentials")
