4b7e9247a8a78552947d315e100c1e26130bbe66
Akshu <akshitagarwal2204@gmail.com>
2021-11-28 16:56:39 +0530

    Add route for Unauthorize

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import authorize
+from .views import authorize, unauthorize
 
 
 urlpatterns = [
     path("authorize/", authorize.AuthViewSet.as_view()),
+    path("unauthorize/", unauthorize.UnAuthViewSet.as_view()),
 ]
---
+++ api/views/unauthorize.py
@@ -0,0 +1,21 @@
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from administration.authentication import UserAuthentication
+
+
+class UnAuthViewSet(APIView):
+    authentication_classes = [UserAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        _response = Response()
+
+        _response.delete_cookie("token")
+        _response.delete_cookie("csrftoken")
+
+        _response.status_code = status.HTTP_205_RESET_CONTENT
+
+        return _response
