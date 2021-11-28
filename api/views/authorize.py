9d25153fe247eeecf017352cdf200b1c050bfad8
Akshu <akshitagarwal2204@gmail.com>
2021-11-28 16:56:21 +0530

    Add GET api/authorize/

--- api/views/authorize.py
+++ api/views/authorize.py
@@ -5,11 +5,12 @@ from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from administration.models import UserModel
+from administration.authentication import CSRFExemptUserAuthentication
 from utils import errors as e
+from utils.permissions import CheckAuthenticatedElseIsAnonymous
 
 
 def get_tokens_for_user(user):
@@ -21,14 +22,32 @@ def get_tokens_for_user(user):
 
 
 class AuthViewSet(APIView):
-    permission_classes = [AllowAny]
+    authentication_classes = [CSRFExemptUserAuthentication]
+    permission_classes = [CheckAuthenticatedElseIsAnonymous]
+
+    def get(self, request, format=None):
+        if request.COOKIES.get(settings.SIMPLE_JWT["AUTH_COOKIE"]) is not None:
+            return Response(
+                {
+                    "status": "ok",
+                    "data": {
+                        "id": request.user.id,
+                        "username": request.user.username,
+                        "email_id": request.user.email_id,
+                        "user_type": request.user.user_type,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return e.HTTP401Response()
 
     def post(self, request, format=None):
         data = request.data
 
         try:
-            email_id = data.get("email_id")
             username = data.get("username")
+            email_id = data.get("email_id")
             password = data.get("password")
 
             if not password:
