baad1509d3055bb328a2ec2b2b0921f398970f4b
Akshu <akshitagarwal2204@gmail.com>
2021-11-28 16:55:42 +0530

    Revoke CSRF - invalid CSRF issue

--- administration/authentication.py
+++ administration/authentication.py
@@ -21,10 +21,30 @@ class UserAuthentication(JWTAuthentication):
 
         validated_token = self.get_validated_token(raw_token)
 
-        check = authentication.CSRFCheck()
-        check.process_request(request)
-        reason = check.process_view(request, None, (), {})
-        if reason:
-            raise PermissionDenied(f"CSRF Failed: {reason}")
+        # check = authentication.CSRFCheck()
+        # check.process_request(request)
+        # reason = check.process_view(request, None, (), {})
+        # if reason:
+        #     raise PermissionDenied(f"CSRF Failed: {reason}")
+
+        return self.get_user(validated_token), validated_token
+
+
+class CSRFExemptUserAuthentication(JWTAuthentication):
+    def authenticate_header(self, request):
+        return "Bearer"
+
+    def authenticate(self, request):
+        header = self.get_header(request)
+
+        if header is None:
+            raw_token = request.COOKIES.get(settings.SIMPLE_JWT["AUTH_COOKIE"]) or None
+        else:
+            raw_token = self.get_raw_token(header)
+
+        if raw_token is None:
+            return None
+
+        validated_token = self.get_validated_token(raw_token)
 
         return self.get_user(validated_token), validated_token
