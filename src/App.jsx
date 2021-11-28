e2d091aea039f873c83ef4f818f9a57a1828c57d
Akshu <akshitagarwal2204@gmail.com>
2021-11-28 16:58:55 +0530

    Customise routes + add constants.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './routes/login';
+import Logout from './routes/logout';
+// import Test from './routes/test';
 import Dashboard from './routes/student/dashboard';
 
 
@@ -11,34 +13,35 @@ function App() {
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route exact path='/' element={<Navigate to='/login' />} />
+                    <Route exact path='/' element={<h1>This is home page</h1>} />
 
                     {/* Login Route */}
-                    <Route exact path="/login" element={<Login />}></Route>
-
+                    <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/logout" element={<Logout />} />
+                    {/* <Route exact path="/test" element={<Test />} /> */}
                     {/* Portals Route - redirecting to dashboard */}
                     {portals.map((r, index) => {
-                        return <Route exact path={r} key={index} element={<Navigate to='dashboard/' />}></Route>
+                        return <Route exact path={r} key={index} element={<Navigate to='dashboard/' />} />
                     })}
 
                     {/* Student */}
-                    <Route path="/student/dashboard" element={<Dashboard />}></Route>
+                    <Route path="/student/dashboard" element={<Dashboard />} />
 
                     {/* Teacher */}
-                    {/* <Route path="/teacher/dashboard" element={<TeacherDashboard />}></Route> */}
+                    {/* <Route path="/teacher/dashboard" element={<TeacherDashboard />} /> */}
 
                     {/* Parent */}
-                    {/* <Route path="/parent/dashboard" element={<ParentDashboard />}></Route> */}
+                    {/* <Route path="/parent/dashboard" element={<ParentDashboard />} /> */}
 
                     {/* Management */}
-                    {/* <Route path="/management/dashboard" element={<ManagementDashboard />}></Route> */}
+                    {/* <Route path="/management/dashboard" element={<ManagementDashboard />} /> */}
 
                     {/* Admin */}
-                    {/* <Route path="/admin/dashboard" element={<AdminDashboard />}></Route> */}
+                    {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
                 </Routes>
             </BrowserRouter>
         </div>
     );
-}
+};
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const BASE_API = "http://localhost:8000/";
+export const SCHOOL_NAME = 'MySchool';
