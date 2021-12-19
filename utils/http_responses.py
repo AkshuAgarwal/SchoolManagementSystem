dab9c2b969fe7f78525fa2ac54f2b2d2dbdd394e
Akshu <akshitagarwal2204@gmail.com>
2021-12-19 13:51:35 +0530

    Rename `utils/errors.py` to `utils/http_responses.py` + add/customize response methods

---
+++ utils/http_responses.py
@@ -0,0 +1,72 @@
+from rest_framework import status as s
+from rest_framework.response import Response
+
+
+def HTTP400Response(message: str = None) -> Response:
+    return Response(
+        {
+            "status": "error",
+            "status_code": 400,
+            "error": {
+                "error_type": "Bad Request",
+                "error_message": message or "Invalid data passed",
+            },
+        },
+        status=s.HTTP_400_BAD_REQUEST,
+    )
+
+
+def HTTP401Response(message: str = None) -> Response:
+    return Response(
+        {
+            "status": "error",
+            "status_code": 401,
+            "error": {
+                "error_type": "Unauthorized",
+                "error_message": message or "Unauthorized",
+            },
+        },
+        status=s.HTTP_401_UNAUTHORIZED,
+    )
+
+
+def HTTP403Response(message: str = None) -> Response:
+    return Response(
+        {
+            "status": "error",
+            "status_code": 403,
+            "error": {
+                "error_type": "Forbidden",
+                "error_message": message or "No Access",
+            },
+        },
+        status=s.HTTP_403_FORBIDDEN,
+    )
+
+
+def HTTP404Response(message: str = None) -> Response:
+    return Response(
+        {
+            "status": "error",
+            "status_code": 404,
+            "error": {
+                "error_type": "NotFound",
+                "error_message": message or "Requested data not found",
+            },
+        },
+        status=s.HTTP_404_NOT_FOUND,
+    )
+
+
+def HTTP500Response(message: str = None) -> Response:
+    return Response(
+        {
+            "status": "error",
+            "status_code": 404,
+            "error": {
+                "error_type": "InternalServerError",
+                "error_message": message or "Some Error Occured. Please try again later",
+            },
+        },
+        status=s.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
