c484d9ef676247e8c3918d9c3a8f1d747a4b0c5e
Akshu <akshitagarwal2204@gmail.com>
2021-12-20 23:19:56 +0530

    ➕ Add media and admin site urlpattern

--- SchoolManagementSystem/urls.py
+++ SchoolManagementSystem/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
+from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
-    path("auth/", include("authentication.urls")),
-]
+    path("admin/", admin.site.urls, name="admin"),
+    path("auth/", include("authentication.urls"), name="auth"),
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
