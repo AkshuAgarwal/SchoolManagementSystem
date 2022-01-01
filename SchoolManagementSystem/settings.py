bc20e2d5c8391f07f9278f4acae4ee32374f0d7f
Akshu <akshitagarwal2204@gmail.com>
2022-01-01 13:45:08 +0530

    🔨 Change token lifetime

--- SchoolManagementSystem/settings.py
+++ SchoolManagementSystem/settings.py
@@ -345,8 +345,8 @@ REST_FRAMEWORK = {
 # https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
