4f74ce8d2ebc9734bcc895c6370ce3f11d2c41ce
Akshu <akshitagarwal2204@gmail.com>
2022-01-20 13:14:10 +0530

    ✨ Add a "password/validate/" endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import authenticate, unauthenticate
 from .views.token import refresh as token_refresh
-from .views.password import reset as password_reset, change as password_change
+from .views.password import reset as password_reset, change as password_change, validate as password_validate
 
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path("token/refresh/", token_refresh.TokenRefreshViewSet.as_view(), name="token_refresh"),
     path("password/reset/", password_reset.PasswordResetViewSet.as_view(), name="password_reset"),
     path("password/change/", password_change.PasswordChangeViewSet.as_view(), name="password_change"),
+    path("password/validate/", password_validate.PasswordValidationViewSet.as_view(), name="password_validate"),
 ]
---
+++ authentication/views/password/validate.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+from root.models import User as UserModel
+from utils.py import http_responses as r
+
+if TYPE_CHECKING:
+    from rest_framework.request import Request
+
+
+class PasswordValidationViewSet(APIView):
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request, format=None):
+        password = request.data.get("password")
+        username = request.data.get("username", None)
+
+        if not password:
+            return r.HTTP400Response("Missing 'password' parameter")
+
+        if not username:
+            user = None
+        else:
+            try:
+                user = UserModel.objects.get(username=username)
+            except UserModel.DoesNotExist:
+                return r.HTTP404Response("No user found with the given username")
+
+        try:
+            validate_password(password, user)
+            return Response(
+                {
+                    "status": "success",
+                    "status_code": status.HTTP_200_OK,
+                },
+                status=status.HTTP_200_OK,
+            )
+        except ValidationError as e:
+            errors = []
+            for error in e:
+                errors.append(str(error))
+
+            return Response(
+                {
+                    "status": "error",
+                    "status_code": status.HTTP_400_BAD_REQUEST,
+                    "error": {
+                        "error_type": "Bad Request",
+                        "error_message": "Validation Failed",
+                        "errors": errors,
+                    },
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
