ad67b0a3e4ec336204353eea96054c2d47595cc3
Akshu <akshitagarwal2204@gmail.com>
2022-01-24 01:41:41 +0530

    🗑 Delete /timetable api (will be added later)

--- api/routes/timetable/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import TimetableModel
-
-
-class TimetableSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TimetableModel
-        exclude = ("id",)
