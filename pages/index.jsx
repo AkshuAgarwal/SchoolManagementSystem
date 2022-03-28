6813350b726d605c779b3ba6ef4e86095ecdba29
Akshu <akshitagarwal2204@gmail.com>
2022-03-28 13:48:42 +0530

    ➕ Add Gallery ImageSlider in home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,17 @@ import getConfig from 'next/config';
 
 import { Container } from '@mui/material';
 
+import { ImageSlider } from '../components';
+
+import galleryMetadata from '../public/images/gallery/_metadata.json';
+
 const { publicRuntimeConfig } = getConfig();
+
+const GALLERY_PATH = '/images/gallery';
+let IMAGES = [];
+galleryMetadata.map(value => {
+    IMAGES.push({ label : value.label, href : `${GALLERY_PATH}/${value.href}` });
+});
 
 export default function Home() {
     return (
@@ -11,8 +21,9 @@ export default function Home() {
             <Head>
                 <title>Home | {publicRuntimeConfig.SCHOOL_NAME}</title>
             </Head>
-            <Container sx={{ margin : 0, display : 'flex', flexDirection : 'column' }}>
-                <p>Index</p>
+            <Container sx={{ display : 'flex', flexDirection : 'column', justifyContent : 'center', alignItems : 'center', padding : '8vh 0' }}>
+                <ImageSlider title="Gallery" images={IMAGES} />
+                <p>This is the home page</p>
             </Container>
         </>
     );
